c949285328aaa93ff51bee6d0171b27b0843b165
TeeVeeoAI <tosi07001@utb.vaxjo.se>
2024-09-04 12:09:23 +0200

    loop uppgifterna

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+/*Console.Write("Enter a number :");
+int tal = int.Parse(Console.ReadLine());
+Console.WriteLine("Printing all even numbers between " + tal + " and 101");
+while (tal <= 101) {
+    if (tal % 2 == 0) {
+        Console.Write(tal);
+        if(tal != 100) {
+            Console.Write(", ");
+        }
+    }
+    tal++;
+} */
+
+Console.Write("Enter a number :");
+int tal = int.Parse(Console.ReadLine());
+Console.WriteLine("Printing all even numbers between " + tal + " and 101");
+while (tal <= 101) {
+    Console.Write(tal);
+    if(tal != 101) {
+        Console.Write(", ");
+    }
+    tal++;
+} 
